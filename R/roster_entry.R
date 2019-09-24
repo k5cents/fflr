49149937d235c84ed36a7bddc5fad9a1486f967d
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-24 13:33:23 -0400

    Use factors for slots in roster_entry()

--- R/roster_entry.R
+++ R/roster_entry.R
@@ -3,54 +3,42 @@
 #' @param entry The `entry` index of `roster` list from a `team` list.
 #' @return A single row tibble roster entry.
 #' @examples
-#' data <- ffl_get(lid = 252353, view = "roster")
+#' data <- ffl_get(lid = 252353, view = "roster", scoringPeriodId = 3)
 #' entry <- data$teams[[5]]$roster$entries[[2]]
 #' roster_entry(entry)
 #' @importFrom purrr map
 #' @importFrom tibble tibble
 #' @importFrom stringr str_which
 #' @importFrom dplyr mutate recode
-#' @importFrom magrittr "%>%"
+#' @importFrom magrittr "%>%" use_series
 #' @export
 roster_entry <- function(entry) {
   stats <- entry$playerPoolEntry$player$stats
-  names(stats) <- purrr::map(stats, `$`, "id")
+  names(stats) <- purrr::map(stats, magrittr::use_series, "id")
   names <- names(stats)
   names(stats)[names == max(names[stringr::str_which(names, "112019")])] <- "proj"
   names(stats)[names == max(names[stringr::str_which(names, "014011")])] <- "score"
   tibble::tibble(
-    year    = stats$score$seasonId,
-    week    = stats$score$scoringPeriodId,
-    player  = as.character(abs(entry$playerId)),
-    first   = entry$playerPoolEntry$player$firstName,
-    last    = entry$playerPoolEntry$player$lastName,
-    pos     = entry$playerPoolEntry$player$defaultPositionId,
-    team    = entry$playerPoolEntry$player$proTeamId,
-    owned   = entry$playerPoolEntry$player$ownership$percentOwned/100,
-    start   = entry$playerPoolEntry$player$ownership$percentStarted/100,
-    slot    = entry$lineupSlotId,
-    proj    = stats$proj$appliedTotal,
-    score   = stats$score$appliedTotal
-  ) %>% dplyr::mutate(
-    slot = slot %>%
-      dplyr::recode(
-        "0"  = "QB",
-        "2"  = "RB",
-        "4"  = "WR",
-        "6"  = "TE",
-        "16" = "DF",
-        "17" = "K",
-        "20" = "BE",
-        "23" = "FX"
-      ),
-    pos = pos %>%
-      dplyr::recode(
-        "1"  = "QB",
-        "2"  = "RB",
-        "3"  = "WR",
-        "4"  = "TE",
-        "5"  = "K",
-        "16" = "D"
-      )
+    year  = stats$score$seasonId,
+    week  = stats$score$scoringPeriodId,
+    owner = entry$playerPoolEntry$onTeamId,
+    slot = factor(
+      x = entry$lineupSlotId,
+      levels = c("0", "2", "4", "6", "16", "17", "20", "23"),
+      labels = c("QB", "RB", "WR", "TE", "DS", "KI", "BE", "FX")
+    ),
+    id    = as.character(abs(entry$playerId)),
+    first = entry$playerPoolEntry$player$firstName,
+    last  = entry$playerPoolEntry$player$lastName,
+    team  = entry$playerPoolEntry$player$proTeamId,
+    pos = factor(
+      x = entry$playerPoolEntry$player$defaultPositionId,
+      levels = c("1", "2", "3", "4", "5", "16"),
+      labels = c("QB", "RB", "WR", "TE", "KI", "DS")
+    ),
+    score = stats$score$appliedTotal,
+    proj  = stats$proj$appliedTotal,
+    start = entry$playerPoolEntry$player$ownership$percentStarted/100,
+    owned = entry$playerPoolEntry$player$ownership$percentOwned/100
   )
 }
