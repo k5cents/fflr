c14c465337eed1e0586a24af3d140d36cc49a8b1
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-02 10:32:58 -0400

    Rename weekly_matchup() to form_matchup()

---
+++ R/form_matchup.R
@@ -0,0 +1,33 @@
+#' @title Format a Weekly Matchup
+#' @description Take a weekly matchup and return tidy tibble of scores.
+#' @param data The list object returned by [fantasy_roster()].
+#' @return A two-row tibble fora matchup's `HOME` and `AWAY` scores.
+#' @examples
+#' data <- ffl_get(lid = 252353, view = "mMatchup")
+#' matchup <- data$schedule[[5]]
+#' weekly_matchup(matchup)
+#' @importFrom tibble tibble
+#' @export
+form_matchup <- function(data) {
+  sched <- data$schedule
+  ind_matchup <- function(matchup) {
+    tibble(
+      game = matchup$id,
+      week = matchup$matchupPeriodId,
+      home = c(FALSE, TRUE),
+      id = c(matchup$away$teamId, matchup$home$teamId),
+      score = c(matchup$away$totalPoints, matchup$home$totalPoints)
+    )
+  }
+  mups <- purrr::map_df(sched, ind_matchup)
+  mups$week <- forcats::as_factor(mups$week)
+  return(mups)
+}
+
+mups %>%
+  right_join(
+    y = form_teams(data = fantasy_members(252353))
+  )
+  ggplot(aes(x = team, y = score)) +
+  geom_col(aes(fill = week)) +
+  coord_flip()
