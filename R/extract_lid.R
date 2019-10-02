f3c4351d5b498e0aec37378dc2bad6ed162edf9b
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-01 21:08:05 -0400

    Create extral_lid() function

---
+++ R/extract_lid.R
@@ -0,0 +1,14 @@
+#' @title Extract ESPN league ID.
+#' @description From any ESPN league page URL (e.g.,
+#'   https://fantasy.espn.com/football/team?leagueId=252353&teamId=6&seasonId=2019),
+#'    extract the league ID digits.
+#' @param url Any ESPN leuage page URL.
+#' @return An ESPN league ID.
+#' @examples
+#' extract_lid("https://fantasy.espn.com/football/team?leagueId=252353&teamId=6&seasonId=2019")
+#' @importFrom stringr str_extract
+#' @export
+extract_lid <- function(url) {
+  stringr::str_extract(url, "(?<=leagueId=)(.*?)(?=&)")
+}
+
