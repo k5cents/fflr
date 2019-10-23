83b5aa2fdb518b303ad31c38ea448c6fd1b7b267
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-22 22:09:30 -0400

    Make form_matchup() to work individually

--- R/form_matchup.R
+++ R/form_matchup.R
@@ -1,27 +1,22 @@
 #' @title Format a Weekly Matchup
 #' @description Take a weekly matchup and return tidy tibble of scores.
-#' @param data The list object returned by [fantasy_roster()].
-#' @return A two-row tibble fora matchup's `HOME` and `AWAY` scores.
+#' @param data The `schedule` element of the list object returned by
+#'   [fantasy_matchup()].
+#' @return A two-row tibble from matchup's `HOME` and `AWAY` scores.
 #' @examples
-#' data <- fantasy_data(lid = 252353, view = "mMatchup")
-#' form_matchup(data)
+#' data <- fantasy_matchup(lid = 252353)
+#' form_matchup(data$schedule[[10]])
 #' @importFrom tibble tibble
 #' @importFrom forcats fct_rev as_factor
 #' @importFrom rlang .data
 #' @export
-form_matchup <- function(data) {
-  sched <- data$schedule
-  ind_matchup <- function(matchup) {
-    tibble(
-      game = matchup$id,
-      week = matchup$matchupPeriodId,
-      home = c(FALSE, TRUE),
-      id = c(matchup$away$teamId, matchup$home$teamId),
-      score = c(matchup$away$totalPoints, matchup$home$totalPoints)
-    )
-  }
-  mups <- purrr::map_df(sched, ind_matchup)
-  mups$week <- as.factor(mups$week)
-  mups <- dplyr::filter(mups, .data$score != 0)
-  return(mups)
+form_matchup <- function(matchup) {
+  tibble(
+    game = matchup$id,
+    week = factor(matchup$matchupPeriodId, levels = 16:1),
+    home = c(FALSE, TRUE),
+    id = c(matchup$away$teamId, matchup$home$teamId),
+    score = c(matchup$away$totalPoints, matchup$home$totalPoints),
+    winner = score == max(score)
+  )
 }
