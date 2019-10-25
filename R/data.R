d21174ebcf8a6b00cebc59a60d10cf30aa925a08
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-24 20:18:52 -0400

    Tweak pro teams tibble docs

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' The ESPN pro team IDs and their NFL abbreviations
 #'
-#' @format A data frame with 32 rows and 2 variables:
+#' @format A tibble with 32 rows and 2 variables:
 #' \describe{
 #'   \item{team}{The ESPN team ID}
 #'   \item{pro}{The NFL team abbreviation}
