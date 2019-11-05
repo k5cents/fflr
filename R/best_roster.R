cd7711f702e795d0b7c35653670ec716388675ce
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-05 13:16:37 -0500

    Warn for current/future weeks in best_roster()

--- R/best_roster.R
+++ R/best_roster.R
@@ -4,7 +4,7 @@
 #' @param roster A roster tibble, as returned by [form_roster()].
 #' @return A tibble with the best roster, excluding bench.
 #' @examples
-#' data <- fantasy_roster(lid = 252353, scoringPeriodId = 2)
+#' data <- fantasy_roster(lid = 252353, scoringPeriodId = 9)
 #' team <- data$teams[[5]]
 #' roster <- form_roster(team)
 #' best_roster(roster)
@@ -12,23 +12,27 @@
 #' @importFrom rlang .data
 #' @export
 best_roster <- function(roster) {
+  if (all(is.na(roster$score))) {
+    stop("The score column is empty, the period is likely the future")
+  }
+  n <- ncol(roster)
   y <- dplyr::filter(roster, .data$slot != "BE")
-  y[, 5:12] <- NA
+  y[, 5:ncol(y)] <- NA
   qb <- sort(roster$score[roster$pos == "QB"], decreasing = T)[1]
-  y[which(y$slot == "QB"), 5:12] <- roster[which(roster$score == qb), 5:12]
+  y[which(y$slot == "QB"), 5:n] <- roster[which(roster$score == qb), 5:n]
   rb <- sort(roster$score[roster$pos == "RB"], decreasing = T)[1:2]
-  y[which(y$slot == "RB"), 5:12] <- roster[which(roster$score %in% rb), 5:12]
+  y[which(y$slot == "RB"), 5:n] <- roster[which(roster$score %in% rb), 5:n]
   wr <- sort(roster$score[roster$pos == "WR"], decreasing = T)[1:2]
-  y[which(y$slot == "WR"), 5:12] <- roster[which(roster$score %in% wr), 5:12]
+  y[which(y$slot == "WR"), 5:n] <- roster[which(roster$score %in% wr), 5:n]
   te <- sort(roster$score[roster$pos == "TE"], decreasing = T)[1]
-  y[which(y$slot == "TE"), 5:12] <- roster[which(roster$score == te), 5:12]
+  y[which(y$slot == "TE"), 5:n] <- roster[which(roster$score == te), 5:n]
   ds <- sort(roster$score[roster$pos == "DS"], decreasing = T)[1]
-  y[which(y$slot == "DS"), 5:12] <- roster[which(roster$score == ds), 5:12]
+  y[which(y$slot == "DS"), 5:n] <- roster[which(roster$score == ds), 5:n]
   ki <- sort(roster$score[roster$pos == "KI"], decreasing = T)[1]
-  y[which(y$slot == "KI"), 5:12] <- roster[which(roster$score == ki), 5:12]
+  y[which(y$slot == "KI"), 5:n] <- roster[which(roster$score == ki), 5:n]
   fx <- sort(roster$score[roster$pos %in% c("RB", "WR", "TE")], decreasing = T)
   fx <- fx[which(!(fx %in% c(rb, wr, te)))][1]
-  y[which(y$slot == "FX"), 5:12] <- roster[which(roster$score %in% fx), 5:12]
+  y[which(y$slot == "FX"), 5:n] <- roster[which(roster$score %in% fx), 5:n]
   return(y)
 }
 
