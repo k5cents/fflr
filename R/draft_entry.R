f8cb95c88d098f4887f35bb39fc1c123200a3d7d
Kiernan Nicholls <kiernann@protonmail.com>
2020-03-14 20:08:23 -0400

    Fix draft documentation

--- R/draft_entry.R
+++ R/draft_entry.R
@@ -1,10 +1,10 @@
 #' Format a single draft pick
 #'
-#' Take a team's roster entry list and return a single row tibble. This function
-#' is used in [form_roster()] to combine the entries from an entire team into a
+#' Take a team's draft list and return a single row tibble. This function
+#' is used in [form_draft()] to combine the entries from an entire team into a
 #' single tibble.
 #'
-#' @param entry The `entry` element of `roster` list from a `team` list.
+#' @param pick The `pick` element of `draft` list from a `team` list.
 #' @return A single row tibble roster entry.
 #' @examples
 #' data <- fantasy_draft(lid = 252353)
