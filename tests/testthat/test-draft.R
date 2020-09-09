1f1fc5b3374040679a5535f12f94ceaf35dac8d4
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-08 22:02:11 -0400

    Test all of draft

--- tests/testthat/test-draft.R
+++ tests/testthat/test-draft.R
@@ -1,6 +1,14 @@
 library(testthat)
 library(fflr)
 
-test_that("draft history returns multiple years", {
-  expect_type(draft_history(252353, TRUE), "list")
+test_that("draft returns for current year", {
+  d <- draft_history(252353, old = FALSE)
+  expect_s3_class(d, "tbl")
+})
+
+test_that("draft returns for past years", {
+  d <- draft_history(252353, old = TRUE)
+  expect_type(d, "list")
+  expect_s3_class(d[[1]], "tbl")
+  expect_length(d, 5)
 })
