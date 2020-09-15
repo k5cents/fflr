6d00d35a29b0f4e239aeec262ec278e2fa5b13b7
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-15 16:44:33 -0400

    Finish standings

---
+++ R/league-standings.R
@@ -0,0 +1,51 @@
+#' League standings
+#'
+#' Return the current and projected standings, win streak, total wins, losses,
+#' points for and against.
+#'
+#' @inheritParams draft_picks
+#' @return A tibble (or list) of league members.
+#' @examples
+#' league_standings(lid = 252353)
+#' @importFrom tibble as_tibble
+#' @export
+league_standings <- function(lid, old = FALSE, ...) {
+  data <- ffl_api(lid, old = old, view = "mTeam", ...)
+  member_cols <- c("user", "owners", "lm")
+  if (old) {
+    out <- rep(list(NA), length(data$teams))
+    for (i in seq_along(data$members)) {
+      out[[i]] <- parse_ranks(data$teams[[i]], year = data$seasonId[i])
+    }
+  } else {
+    out <- parse_ranks(data$teams, year = data$seasonId)
+  }
+  return(out)
+}
+
+parse_ranks <- function(teams, year = NULL, week = NULL) {
+  record <- teams$record$overall[, c(1, 2, 8:9, 3:7)]
+  record_nms <- c("back", "against", "points", "streak", "type")
+  names(record)[c(1, 6:9)] <- record_nms
+  x <- tibble::tibble(
+    year = year,
+    team = teams$id,
+    abbrev = teams$abbrev,
+    draft = teams$draftDayProjectedRank,
+    current = teams$currentProjectedRank,
+    seed = teams$playoffSeed,
+    final = teams$rankCalculatedFinal,
+    record
+  )
+  x$percentage <- round(x$percentage, 3)
+  for (j in seq_along(x)[-11]) {
+    if (!is.numeric(x[[j]]) | all(is.na(x[[j]]))) {
+      next()
+    } else if(all(x[[j]] == 0)) {
+      x[[j]] <- NA_integer_
+    }
+  }
+  x$streak[x$type == "LOSS"] <- -1 * x$streak[x$type == "LOSS"]
+  x$type <- NULL
+  return(x)
+}
---
+++ tests/testthat/test-standings.R
@@ -0,0 +1,18 @@
+library(testthat)
+library(fflr)
+
+test_that("standings returns for current year", {
+  s <- league_standings(252353, old = FALSE)
+  expect_s3_class(s, "tbl")
+  expect_length(s, 15)
+})
+
+test_that("standings returns for past years", {
+  s <- league_standings(252353, old = TRUE)
+  expect_type(s, "list")
+  expect_s3_class(s[[1]], "tbl")
+  expect_length(s, 5)
+  expect_true(all(is.na(s[[1]]$draft)))
+  expect_true(all(!is.na(s[[5]]$draft)))
+  expect_true(all(is.na(s[[5]]$current)))
+})
