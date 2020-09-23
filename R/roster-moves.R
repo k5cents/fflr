d7f15160b409fa97edc6a4948c1251d25bb4f093
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-23 14:02:16 -0400

    Finish moves scraping
    Progress on #5

---
+++ R/roster-moves.R
@@ -0,0 +1,45 @@
+#' Roster moves
+#'
+#' The individual proposed and executed transactions, trades, and waiver moves.
+#'
+#' @inheritParams draft_picks
+#' @return A tibble of transactions and roster moves.
+#' @examples
+#' roster_moves(lid = 252353)
+#' @importFrom tibble as_tibble
+#' @export
+roster_moves <- function(lid, old = FALSE, ...) {
+  data <- ffl_api(lid, old = old, view = "mTransactions2", ...)
+  if (old) {
+    stop("not currently supported for past seasons")
+  } else {
+    parse_moves(data$transactions, y = data$seasonId)
+  }
+}
+
+ffl_date <- function(date) {
+  as.POSIXct(date/1000, origin = "1970-01-01")
+}
+
+parse_moves <- function(t, y = NULL) {
+  for (i in seq_along(t$items)) {
+    if (!is.null(t$items[[i]])) {
+      t$items[[i]] <- cbind(tx = t$id[i], t$items[[i]])
+    }
+  }
+  i <- do.call("rbind", t$items)[, c(-4, -5)]
+  i_nm <- c("from_slot", "from_team", "id", "to_slot", "to_team", "move")
+  names(i)[2:7] <- i_nm
+  i$from_slot[i$from_slot == -1L] <- NA_integer_
+  i$from_team[i$from_team == 0] <- NA_integer_
+  i$to_slot[i$to_slot == -1L] <- NA_integer_
+  t <- t[, c(11, 14, 1, 12, 9, 13, 3)]
+  names(t) <- c("week", "type", "bid", "status", "date", "team", "tx")
+  t$date <- ffl_date(t$date)
+  t$bid[t$bid == 0] <- NA_integer_
+  t$status[t$status == "FAILED_INVALIDPLAYERSOURCE"] <- "FAILED"
+  t <- cbind(year = y, t)
+  x <- merge(t, i, by = "tx")
+  x$tx <- substr(x$tx, start = 1, stop = 8)
+  return(tibble::as_tibble(x))
+}
---
+++ tests/testthat/test-moves.R
@@ -0,0 +1,14 @@
+library(testthat)
+library(fflr)
+
+test_that("movies returns for current year", {
+  m <- roster_moves(252353, old = FALSE)
+  expect_s3_class(m, "tbl")
+  expect_length(m, 14)
+  expect_s3_class(m$date, "POSIXct")
+  expect_equal(unique(nchar(m$tx)), 8)
+})
+
+test_that("moves error for past years", {
+  expect_error(roster_moves(252353, old = TRUE))
+})
