c596097ee9b67ae0f27b8861cf22abc9358a1b9a
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-24 23:31:10 -0400

    Allow user to set ID option

---
+++ tests/testthat/test-lid.R
@@ -0,0 +1,31 @@
+library(testthat)
+library(fflr)
+library(fs)
+
+test_that("existing league ID can be retrieved", {
+  options(lid = 252353)
+  expect_message(set_lid(252353), "already")
+})
+
+test_that("existing league ID can be retrieved", {
+  options(lid = NULL)
+  expect_message(set_lid(252353), "^new")
+})
+
+rp <- path.expand("~/.Rprofile")
+if (!requireNamespace("usethis", quietly = TRUE)) {
+  test_that(".Rprofile not edited without 'usethis'", {
+    expect_error(set_lid(252353, edit = TRUE))
+  })
+} else if (!file.exists(rp)) {
+  test_that(".Rprofile is not created if exists", {
+    set_lid(252353, edit = TRUE)
+    expect_true(file.exists(rp))
+  })
+} else {
+  test_that("", {
+    options(lid = NULL)
+    x <- set_lid(252353, edit = TRUE)
+    expect_equal(x, 252353)
+  })
+}
