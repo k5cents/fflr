90562bedb754cb14c78c2318bbe4cb0d9db13836
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-26 21:48:41 -0400

    Add week to transaction summary

--- R/league-transactions.R
+++ R/league-transactions.R
@@ -14,22 +14,30 @@ league_transactions <- function(lid = getOption("lid"), old = FALSE, ...) {
   if (old) {
     out <- rep(list(NA), length(data$teams))
     for (i in seq_along(data$members)) {
-      out[[i]] <- parse_trans(data$teams[[i]], year = data$seasonId[i])
+      out[[i]] <- parse_trans(
+        teams = data$teams[[i]],
+        y = data$seasonId[i],
+        w = data$scoringPeriodId[i]
+      )
     }
   } else {
-    out <- parse_trans(data$teams, year = data$seasonId)
+    out <- parse_trans(
+      teams = data$teams,
+      y = data$seasonId,
+      w = data$scoringPeriodId
+    )
   }
   return(out)
 }
 
-parse_trans <- function(teams, year = NULL, week = NULL) {
+parse_trans <- function(teams, y = NULL, w = NULL) {
   teams$transactionCounter$matchupAcquisitionTotals <- NULL
   counter <- teams$transactionCounter[, c(1:3, 5:6, 9)]
   names(counter)[c(1, 4:5)] <- c("spent", "activated", "reserved")
   x <- tibble::tibble(
-    year = year,
+    year = y,
+    week = w,
     team = teams$id,
-    abbrev = teams$abbrev,
     waiver = teams$waiverRank,
     counter
   )
--- tests/testthat/test-transactions.R
+++ tests/testthat/test-transactions.R
@@ -4,13 +4,13 @@ library(fflr)
 test_that("rosters return for current year", {
   t <- league_transactions(252353, old = FALSE)
   expect_s3_class(t, "tbl")
-  expect_length(t, 10)
+  expect_length(t, 11)
 })
 
 test_that("rosters return for past years", {
   t <- league_transactions(252353, old = TRUE)
   expect_type(t, "list")
   expect_length(t, 5)
-  expect_length(t[[1]], 10)
+  expect_length(t[[1]], 11)
   expect_s3_class(t[[1]], "tbl")
 })
