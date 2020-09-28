377cd6145734c6ef6edfd31396a96d9a1fec17a3
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-27 23:16:36 -0400

    Skip live scoring
    Wait for #9

--- tests/testthat/test-live.R
+++ tests/testthat/test-live.R
@@ -2,13 +2,9 @@ library(testthat)
 library(fflr)
 
 test_that("live scoreboard works if gameday", {
-  wday <- weekdays(Sys.Date(), abbreviate = TRUE)
-  if (wday %in% c("Mon", "Tue", "Wed", "Thur")) {
-    skip("no live scoring until gameday")
-  } else {
-    s <- live_scoring(252353)
-    expect_length(s, 5)
-    expect_equal(nrow(s), 8)
-  }
+  skip("no live scoring until gameday")
+  s <- live_scoring(252353)
+  expect_length(s, 5)
+  expect_equal(nrow(s), 8)
   expect_error(live_scoring(252353, old = TRUE))
 })
