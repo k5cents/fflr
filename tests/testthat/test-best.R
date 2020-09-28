24fdb5025013d237e6cdc78ada82fa59430090eb
Kiernan Nicholls <kiernann@protonmail.com>
2020-09-27 23:30:07 -0400

    Fix length test for acquisition
    Changes made in fecd5f1ac4e51be6b8e9b9f0f0c9db901171156b

--- tests/testthat/test-best.R
+++ tests/testthat/test-best.R
@@ -9,7 +9,7 @@ test_that("can calculate single best roster", {
   r <- team_roster(252353, old = FALSE, week = 1)
   b <- best_roster(r[[1]])
   expect_s3_class(b, "tbl")
-  expect_length(b, 15)
+  expect_length(b, 17)
   expect_height(b, 9)
   expect_type(roster_score(b), "double")
 })
