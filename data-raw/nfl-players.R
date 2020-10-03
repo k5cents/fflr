bbc848d0f35023477a6c744d77f548fc2f8f4881
Kiernan Nicholls <kiernann@protonmail.com>
2020-10-03 14:48:08 -0400

    Sort NFL player alphabetically

--- data-raw/nfl-players.R
+++ data-raw/nfl-players.R
@@ -1,3 +1,4 @@
 ## code to prepare `players` dataset goes here
 nfl_players <- fflr::all_players()[, 3:7]
+nfl_players <- nfl_players[order(nfl_players$last, nfl_players$first), ]
 usethis::use_data(nfl_players, overwrite = TRUE)
