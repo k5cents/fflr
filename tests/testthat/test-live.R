11db8d8a895ce2cdebe61f9ac573a10cb7298f29
Kiernan Nicholls <kiernann@protonmail.com>
2020-10-04 12:49:24 -0400

    Test live scoring conditionally

--- tests/testthat/test-live.R
+++ tests/testthat/test-live.R
@@ -2,9 +2,12 @@ library(testthat)
 library(fflr)
 
 test_that("live scoreboard works if gameday", {
-  skip("no live scoring until gameday")
   s <- live_scoring(252353)
-  expect_length(s, 5)
-  expect_equal(nrow(s), 8)
-  expect_error(live_scoring(252353, old = TRUE))
+  if (all(s$score == 0) | any(is.na(s$score) | nrow(s) < 8)) {
+    skip("no live scoring until gameday")
+  } else {
+    expect_length(s, 5)
+    expect_equal(nrow(s), 8)
+    expect_error(live_scoring(252353, old = TRUE))
+  }
 })
