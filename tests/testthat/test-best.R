c9c62e94f44248cc8b500bc2d22994d7242eeff5
Kiernan Nicholls <kiernann@protonmail.com>
2020-10-10 19:22:12 -0400

    Fix best test for acquire removal

--- tests/testthat/test-best.R
+++ tests/testthat/test-best.R
@@ -10,10 +10,10 @@ test_that("scores stop if pre-game", {
   b <- best_roster(r)
   expect_s3_class(b, "tbl")
   if (sum(is.na(r$score)) > 3) {
-    expect_length(b, 16)
+    expect_length(b, 14)
     expect_height(b, 9)
     expect_gt(roster_score(b), roster_score(r))
   } else {
-    expect_length(b, 17)
+    expect_length(b, 15)
   }
 })
