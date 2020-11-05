8b2f4b249341f89db95bf36fb3676c2fcd99514e
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-05 09:47:56 -0500

    Skip all_players() test on CRAN

--- tests/testthat/test-players.R
+++ tests/testthat/test-players.R
@@ -2,6 +2,7 @@ library(testthat)
 library(fflr)
 
 test_that("all players can be listed without error", {
+  skip_on_cran()
   p <- all_players(lid = NULL)
   expect_s3_class(p, "tbl")
   expect_length(p, 19)
