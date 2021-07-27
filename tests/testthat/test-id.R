90b53d26291c1b75bc9cc78dccdb13c5ee3229bd
Kiernan Nicholls <kiernann@protonmail.com>
2021-07-27 11:42:31 -0400

    Test for empty ID return

--- tests/testthat/test-id.R
+++ tests/testthat/test-id.R
@@ -9,6 +9,14 @@ test_that("league ID can be supplied manually", {
   expect_equal(id, "252353")
 })
 
+test_that("league ID returns empty if not found", {
+  skip_if_not(nzchar(Sys.getenv("ESPN_LEAGUE_ID")))
+  old_id <- Sys.getenv("ESPN_LEAGUE_ID")
+  Sys.unsetenv("ESPN_LEAGUE_ID")
+  expect_warning(nzchar(fflr_id()))
+  Sys.setenv("ESPN_LEAGUE_ID" = old_id)
+})
+
 test_that("league ID can be extracted from URL", {
   id <- fflr_id("https://fantasy.espn.com/football/team?leagueId=252353")
   expect_equal(id, "252353")
