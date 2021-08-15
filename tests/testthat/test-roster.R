40e058bf121f2d70f74a62556527aa8a43f7162f
Kiernan Nicholls <kiernann@protonmail.com>
2021-08-15 17:10:01 -0400

    Return list of current team rosters
    TODO: Add `scoringPeriodId` argument to get previous rosters?
    
    Progress to #24

---
+++ tests/testthat/test-roster.R
@@ -0,0 +1,18 @@
+test_that("All current team rosters as list", {
+  r <- team_roster(leagueId = "42654852")
+  expect_type(r, "list")
+  expect_length(r, 4)
+  expect_s3_class(r[[1]], "data.frame")
+  expect_length(r[[1]], 15)
+  expect_s3_class(r[[1]]$slot, "factor")
+})
+
+test_that("All final period team rosters as list of list", {
+  rh <- team_roster(leagueId = "252353", leagueHistory = TRUE)
+  expect_gt(length(rh), 4)
+  expect_length(rh[[1]], 8)
+  expect_length(rh[[2]], 10)
+  expect_type(rh, "list")
+  expect_type(rh[[1]], "list")
+  expect_s3_class(rh[[1]][[1]], "data.frame")
+})
