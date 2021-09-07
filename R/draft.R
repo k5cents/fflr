c07601ec9d5f5591a5b5378a525580b0b91f2e06
Kiernan Nicholls <kiernann@protonmail.com>
2021-09-06 23:24:41 -0400

    Add `draft_picks()`
    Progress towards #24

---
+++ R/draft.R
@@ -0,0 +1,60 @@
+#' Fantasy draft history
+#'
+#' Return the sequential result of a fantasy draft pick, whether snake or
+#' salary cap format.
+#'
+#' @inheritParams ffl_api
+#' @return A data frame(s) of draft picks.
+#' @examples
+#' draft_picks(leagueId = "42654852")
+#' draft_picks(leagueId = "252353", leagueHistory = TRUE)
+#' @export
+draft_picks <- function(leagueId = ffl_id(), leagueHistory = FALSE, ...) {
+  dat <- ffl_api(
+    leagueId = leagueId,
+    view = c("mDraftDetail", "mTeam"),
+    leagueHistory = leagueHistory,
+    ...
+  )
+  if (leagueHistory) {
+    out <- rep(list(NA), length(dat$seasonId))
+    for (i in seq_along(out)) {
+      tm <- data.frame(
+        stringsAsFactors = FALSE,
+        id = dat$teams[[i]]$id,
+        abbrev = factor(
+          x = dat$teams[[i]]$abbrev,
+          levels = dat$teams[[i]]$abbrev
+        )
+      )
+      x <- dat$draftDetail$picks[[i]]
+      x$autoDraftTypeId <- as.logical(x$autoDraftTypeId)
+      x$nominatingTeamId <- team_abbrev(x$nominatingTeamId, teams = tm)
+      x$teamId <- team_abbrev(x$teamId, teams = tm)
+      x$seasonId <- dat$seasonId[i]
+      x <- x[c(length(x), seq(x) - 1)]
+      x$bidAmount[x$bidAmount == 0] <- NA_integer_
+      out[[i]] <- as_tibble(x)
+    }
+    return(out)
+  } else {
+    x <- dat$draftDetail$picks
+    x$autoDraftTypeId <- as.logical(x$autoDraftTypeId)
+    tm <- data.frame(
+      stringsAsFactors = FALSE,
+      id = dat$teams$id,
+      abbrev = factor(
+        x = dat$teams$abbrev,
+        levels = dat$teams$abbrev
+      )
+    )
+    x <- dat$draftDetail$picks
+    x$autoDraftTypeId <- as.logical(x$autoDraftTypeId)
+    x$nominatingTeamId <- team_abbrev(x$nominatingTeamId, teams = tm)
+    x$teamId <- team_abbrev(x$teamId, teams = tm)
+    x$seasonId <- dat$seasonId[i]
+    x <- x[c(length(x), seq(x) - 1)]
+    x$bidAmount[x$bidAmount == 0] <- NA_integer_
+    as_tibble(x)
+  }
+}
