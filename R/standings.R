b41a7aa5ae39b4ceb37a899f3244ba8d5381f5c7
Kiernan Nicholls <kiernann@protonmail.com>
2021-09-14 11:00:22 -0400

    Add `league_simulation()`

--- R/standings.R
+++ R/standings.R
@@ -10,7 +10,12 @@
 #' @importFrom tibble tibble
 #' @export
 league_standings <- function(leagueId = ffl_id(), leagueHistory = FALSE, ...) {
-  dat <- ffl_api(leagueId, leagueHistory, view = "mTeam", ...)
+  dat <- ffl_api(
+    leagueId,
+    leagueHistory,
+    view = c("mTeam", "mStandings", "mSettings"),
+    ...
+  )
   if (leagueHistory) {
     out <- rep(list(NA), length(dat$teams))
     for (i in seq_along(dat$members)) {
@@ -32,14 +37,70 @@ league_standings <- function(leagueId = ffl_id(), leagueHistory = FALSE, ...) {
 
 parse_ranks <- function(teams, y = NULL, w = NULL) {
   tibble::tibble(
-    year = y,
-    week = w,
-    team = teams$id,
+    seasonId = y,
+    scoringPeriodId = w,
+    teamId = teams$id,
     abbrev = factor(teams$abbrev, levels = teams$abbrev),
     draftDayProjectedRank = teams$draftDayProjectedRank,
     currentProjectedRank = teams$currentProjectedRank,
     playoffSeed = teams$playoffSeed,
     rankCalculatedFinal = teams$rankCalculatedFinal,
-    teams$record$overall
+    teams$record$overall,
+    playoffPct = teams$currentSimulationResults$playoffPct,
+    divisionWinPct = teams$currentSimulationResults$divisionWinPct
+  )
+}
+
+#' League standing simulation
+#'
+#' The ESPN algorithm simulates the entire season according to the projection
+#' and matchup schedule to calculate the probability of a team winning their
+#' division and making the playoffs.
+#'
+#' @inheritParams ffl_api
+#' @return A data frame of simulated team standings.
+#' @examples
+#' league_simulation(leagueId = "42654852")
+#' @importFrom tibble tibble
+#' @export
+league_simulation <- function(leagueId = ffl_id(), leagueHistory = FALSE, ...) {
+  dat <- ffl_api(
+    leagueId,
+    leagueHistory,
+    view = c("mTeam", "mStandings"),
+    ...
+  )
+  if (leagueHistory) {
+    out <- rep(list(NA), length(dat$teams))
+    for (i in seq_along(dat$members)) {
+      out[[i]] <- parse_sim(
+        teams = dat$teams[[i]],
+        y = dat$seasonId[i],
+        w = dat$scoringPeriodId[i]
+      )
+    }
+  } else {
+    out <- parse_sim(
+      teams = dat$teams,
+      y = dat$seasonId,
+      w = dat$scoringPeriodId
+    )
+  }
+  return(out)
+}
+
+parse_sim <- function(teams, y = NULL, w = NULL) {
+  tibble::tibble(
+    seasonId = y,
+    scoringPeriodId = dat$settings$scheduleSettings$matchupPeriodCount,
+    teamId = teams$id,
+    abbrev = factor(teams$abbrev, levels = teams$abbrev),
+    draftDayProjectedRank = teams$draftDayProjectedRank,
+    currentProjectedRank = teams$currentProjectedRank,
+    losses = teams$currentSimulationResults$modeRecord$losses,
+    ties = teams$currentSimulationResults$modeRecord$ties,
+    wins = teams$currentSimulationResults$modeRecord$wins,
+    playoffPct = teams$currentSimulationResults$playoffPct,
+    divisionWinPct = teams$currentSimulationResults$divisionWinPct
   )
 }
