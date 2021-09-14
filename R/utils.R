e5263ca9b973fecb130d60c4cca869cae7f0c373
Kiernan Nicholls <kiernann@protonmail.com>
2021-09-14 18:10:42 -0400

    Add skip/stop utility for pre-draft

--- R/utils.R
+++ R/utils.R
@@ -56,3 +56,20 @@ ffl_merge <- function(x, y, ...) {
   out <- merge(x, y, sort = FALSE, ...)[, union(names(x), names(y))]
   as_tibble(out)
 }
+
+stop_predraft <- function(dat) {
+  if ("draftDetail" %in% names(dat)) {
+    if (!dat$draftDetail$drafted) {
+      message("This league has not drafted and the information is unavailable")
+      return(data.frame())
+    }
+  }
+}
+
+skip_predraft <- function(dat) {
+  if (is_installed("testthat") & "draftDetail" %in% names(dat)) {
+    if (!dat$draftDetail$drafted) {
+      testthat::skip(message = "This league has not drafted")
+    }
+  }
+}
