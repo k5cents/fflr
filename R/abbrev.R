68fc9b7853e9a6c62434cdca2daa208219c12847
Kiernan Nicholls <kiernann@protonmail.com>
2021-10-12 00:10:59 -0400

    Add abbreviating utils for stats

--- R/abbrev.R
+++ R/abbrev.R
@@ -1,27 +1,20 @@
-# position                    abbrev  slot  pos
-# Quarterback                 QB      0     1
-# Team Quarterback            TQB     1
-# Running Back                RB      2     2
-# Running Back/Wide Receiver  RB/WR   3
-# Wide Receiver               WR      4     3
-# Wide Receiver/Tight End     WR/TE   5
-# Tight End                   TE      6     4
-# Flex                        FLEX    23
-# Offensive Player Utility    OP      7
-# Defensive Tackle            DT      8     9
-# Defensive End               DE      9     10
-# Linebacker                  LB      10    11
-# Defensive Line              DL      11
-# Cornerback                  CB      12    12
-# Safety                      S       13    13
-# Defensive Back              DB      14
-# Defensive Player Utility    DP      15
-# Team Defense/Special Teams  D/ST    16    16
-# Place Kicker                K       17    5
-# Punter                      P       18    7
-# Head Coach                  HC      19    14
-# Bench                       BE      20
-# Injured Reserve             IR      21
+#' Convert team ID to abbreviation
+#'
+#' @param teamId A integer vector of team numbers to convert.
+#' @param teams A table of teams, like that from [league_teams()].
+#' @return A factor vector of team abbreviations.
+#' @examples
+#' team_abbrev(teamId = 2, teams = league_teams(leagueId = "42654852"))
+#' @export
+team_abbrev <- function(teamId, teams = league_teams(leagueId = ffl_id())) {
+  if (!(all(c("abbrev", "teamId") %in% names(teams)))) {
+    stop("team data lacks `abbrev` and `teamId` columns")
+  }
+  stopifnot(is.data.frame(teams))
+  teams$abbrev[match(teamId, teams$teamId)]
+}
+
+# -------------------------------------------------------------------------
 
 slot_abbrev <- function(slot) {
   stopifnot(could_be_numeric(slot))
@@ -37,6 +30,8 @@ slot_unabbrev <- function(abbrev) {
     stop("Abbreviation not found for this roster slot")
   }
 }
+
+# -------------------------------------------------------------------------
 
 pos_abbrev <- function(pos) {
   pos_abbrev <- pos_ids$abbrev[!is.na(pos_ids$position)]
@@ -54,6 +49,8 @@ pos_unabbrev <- function(abbrev) {
   }
 }
 
+# -------------------------------------------------------------------------
+
 pro_abbrev <- function(proTeamId) {
   fflr::nfl_teams$abbrev[match(proTeamId, fflr::nfl_teams$proTeamId)]
 }
@@ -68,18 +65,20 @@ pro_unabbrev <- function(abbrev) {
   }
 }
 
-#' Convert team ID to abbreviation
-#'
-#' @param teamId A integer vector of team numbers to convert.
-#' @param teams A table of teams, like that from [league_teams()].
-#' @return A factor vector of team abbreviations.
-#' @examples
-#' team_abbrev(teamId = 2, teams = league_teams(leagueId = "42654852"))
-#' @export
-team_abbrev <- function(teamId, teams = league_teams(leagueId = ffl_id())) {
-  if (!(all(c("abbrev", "teamId") %in% names(teams)))) {
-    stop("team data lacks `abbrev` and `teamId` columns")
+# -------------------------------------------------------------------------
+
+stat_abbrev <- function(statId) {
+  stat_abbrev <- stat_ids$statAbbrev[!is.na(stat_ids$statAbbrev)]
+  stopifnot(could_be_numeric(statId))
+  stat_ids$statAbbrev[match(statId, stat_ids$statId)]
+}
+
+stat_unabbrev <- function(abbrev) {
+  if (all(could_be_numeric(abbrev))) {
+    return(as.integer(abbrev))
+  } else if (is.character(abbrev) && abbrev %in% stat_ids$statAbbrev) {
+    stat_ids$statId[match(abbrev, stat_ids$statAbbrev)]
+  } else {
+    stop("Abbreviation not found for position")
   }
-  stopifnot(is.data.frame(teams))
-  teams$abbrev[match(teamId, teams$teamId)]
 }
