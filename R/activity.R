f7821818dbe80de2e1e316076a9766aa9b1a607b
Kiernan Nicholls <kiernann@protonmail.com>
2021-10-27 16:18:01 -0400

    Simplify `recent_activity()` with nested items
    Trade activity from the API is still wonky

--- R/activity.R
+++ R/activity.R
@@ -23,33 +23,11 @@ recent_activity <- function(leagueId = ffl_id(), leagueHistory = FALSE,
   } else if (is.null(dat$transactions)) {
     message("no roster moves for current period")
     return(data.frame())
-  } else {
-    t <- dat$transactions
-    for (i in seq_along(t$items)) {
-      if (!is.null(t$items[[i]]) & length(t$items[[i]]) > 0) {
-        t$items[[i]] <- cbind(id = t$id[i], t$items[[i]])
-      }
-    }
-    i <- do.call("rbind", t$items)[, -c(4, 5)]
-    i$fromLineupSlotId[i$fromLineupSlotId == -1L] <- NA_integer_
-    i$fromLineupSlotId <- slot_abbrev(i$fromLineupSlotId)
-    i$toLineupSlotId <- slot_abbrev(i$toLineupSlotId)
-    i$fromTeamId[i$fromTeamId == 0] <- NA_integer_
-    i$toLineupSlotId[i$toLineupSlotId == -1L] <- NA_integer_
-    i$toLineupSlotId[i$toLineupSlotId == 0] <- NA_integer_
-    t$bidAmount[t$bidAmount == 0] <- NA_integer_
-    t$proposedDate <- ffl_date(t$proposedDate)
-    t$processDate <- ffl_date(t$processDate)
-    t$seaonId <- dat$seasonId
-    t <- t[, c("seaonId", "scoringPeriodId", "proposedDate", "processDate",
-               "bidAmount", "status", "id")]
-    t <- merge(t, i, by = "id")
-    t$id <- substr(t$id, start = 1, stop = 8)
-    t$bidAmount[t$type == "DROP"] <- NA
-    t$toTeamId[t$toTeamId == 0] <- NA
-    t$fromTeamId[t$fromTeamId == 0] <- NA
-    t$abbrev <- team_abbrev(t$toTeamId, teams = tm)
-    t <- move_col(t, "abbrev", 13)
-    as_tibble(t[order(t$proposedDate), ])
   }
+  t <- dat$transactions
+  t$processDate <- ffl_date(t$processDate)
+  t$proposedDate <- ffl_date(t$proposedDate)
+  t$rating <- NULL
+  t$subOrder <- NULL
+  as_tibble(t)
 }
