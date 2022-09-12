159ac5e49c516fa966b85525862b0a73881adf90
Kiernan Nicholls <kiernann@protonmail.com>
2022-09-12 19:59:06 -0400

    Update `nfl_players` for the 2022 season

--- data-raw/nfl-players.R
+++ data-raw/nfl-players.R
@@ -3,12 +3,12 @@ library(tidyverse)
 library(lubridate)
 library(fflr)
 
-nfl_players <- all_players(limit = NULL)[, 3:7]
-out_players <- filter(nfl_players, defaultPositionId != "DS")
+nfl_players <- list_players(limit = NULL)[, 3:7]
+out_players <- filter(nfl_players, defaultPosition != "D/ST")
 out <- rep(list(NULL), nrow(out_players))
 pb <- txtProgressBar(max = nrow(out_players), style = 3)
 for (i in seq_along(out_players$id)) {
-  if (is.na(out_players$defaultPositionId[i])) {
+  if (is.na(out_players$defaultPosition[i])) {
     next()
   } else {
     out[[i]] <- player_info(out_players$id[i])
@@ -25,13 +25,14 @@ out_players$dateOfBirth <- as.Date(
 
 out_players <- select(out_players, -positionId)
 
-def_players <- filter(nfl_players, defaultPositionId == "DS")
+def_players <- filter(nfl_players, defaultPosition == "D/ST")
 
-nfl_players <- select(nfl_players, id, proTeam, defaultPositionId)
+nfl_players <- select(nfl_players, id, proTeam, defaultPosition)
 zzz <- inner_join(nfl_players, out_players, by = "id")
-zzz <- relocate(zzz, proTeam, defaultPositionId, .after = lastName)
+zzz <- relocate(zzz, proTeam, defaultPosition, .after = lastName)
 
 nfl_players <- bind_rows(zzz, def_players)
 nfl_players <- select(nfl_players, -birthPlace, -draftSelection)
 names(nfl_players)[1] <- "playerId"
 usethis::use_data(nfl_players, overwrite = TRUE)
+readr::write_csv(nfl_players, "data-raw/nfl_players.csv", na = "")
