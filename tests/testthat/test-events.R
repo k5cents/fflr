a05f0bc504f680a5d1f44fc6c24a3eead924bc75
Kiernan Nicholls <kiernann@protonmail.com>
2022-09-18 20:44:03 -0400

    Add test for `pro_scores()`

--- tests/testthat/test-events.R
+++ tests/testthat/test-events.R
@@ -2,4 +2,12 @@ test_that("get NFL game schedule times", {
   e <- pro_events()
   expect_s3_class(e, "data.frame")
   expect_type(e$competitors, "list")
+  expect_length(e, 13)
+})
+
+test_that("get NFL game scores", {
+  s <- pro_scores()
+  expect_s3_class(s, "data.frame")
+  expect_type(s$lineup, "list")
+  expect_length(s, 9)
 })
